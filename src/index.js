3d7c82ef9a95ebb645f9aa55ededc32c2eaa7a8c
unknown <jefferyamon2468@gmail.com>
2025-12-30 14:08:49 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import booksRoutes from "./routes/booksRoutes.js";
+import { connectDb } from "./lib/db.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", booksRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+  connectDb();
+});
